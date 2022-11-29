21b0f7e8de7fef8bb26ab9e8f6fc85507afd881b
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-11-29 09:17:52 -0500

    Update vrx_ct_dump_to_s3.sh

--- vrx_ct_dump_to_s3.sh
+++ vrx_ct_dump_to_s3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir viquity-database-import-us-east-1/Jobs/aact/dump
 mv *.txt dump
-
-aws s3 sync dump/ s3://viquity-database-import-us-east-1/Jobs/aact/dump-"$(date +%d-%m)"/
+aws s3 sync viquity-database-import-us-east-1/Jobs/aact/dump/ s3://viquity-database-import-us-east-1/Jobs/aact/dump-"$(date +%d-%m)"/
 
