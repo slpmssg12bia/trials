60a4c30e518432dc96d3e42c8553acf6518fa7ee
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-12-03 17:34:46 -0500

    Update trials_clean.sh

--- trials_clean.sh
+++ trials_clean.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 rm -rf dump
 rm db.zip
-
-
-#must be manually created in unbuntu
-#touch trials_clean.sh
-#nano trials_clean.sh
-#enter code above
-#ctrl X
-#Y
-#chmod +x trials_clean.sh
