6f26dabdd39ef1c5b383e6cb1344af632ec2b684
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-12-27 09:13:34 -0500

    Update dump_to_s3.sh

--- dump_to_s3.sh
+++ dump_to_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir trialsdump
 mv *.txt trialsdump
-aws s3 sync trialsdump/ s3://viquity-database-import-us-east-1/Jobs/aact/aact_current_dump/trialsdump-"$(date + %d-%m-%H-%M)"/
+aws s3 sync trialsdump/ s3://viquity-database-import-us-east-1/Jobs/aact/aact_current_dump/trialsdump/
